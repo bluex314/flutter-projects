0e952bfc4df0c13670e8523b35cbfa41a17c7a6c
bluex314 <manuelxantony@gmail.com>
2021-04-19 18:47:37 +0530

    initial check in

---
+++ design_challenge_1/android/app/src/main/kotlin/com/blue314/design_challenge_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.blue314.design_challenge_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ design_challenge_1/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 100,
+                height: double.infinity,
+                color: Colors.red,
+                child: Text(''),
+              ),
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(width: 100, height: 100, color: Colors.yellow),
+                    Container(width: 100, height: 100, color: Colors.green),
+                  ],
+                ),
+              ),
+              Container(
+                width: 100,
+                height: double.infinity,
+                color: Colors.blue,
+                child: Text(''),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
